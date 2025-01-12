01c03d55d85cc50dd42e34ac5d92fa1f6594d7b2
Kylong <kylong@zjl.me>
2025-01-13 02:06:30 +0800

    test: 💍 finish errors exercises

--- exercises/03_if/if1.rs
+++ exercises/03_if/if1.rs
@@ -4,9 +4,9 @@ fn bigger(a: i32, b: i32) -> i32 {
     // Do not use:
     // - another function call
     // - additional variables
-    if (a == b) {
+    if a == b {
         return a;
-    } else if (a > b) {
+    } else if a > b {
         return a;
     } else {
         return b;
